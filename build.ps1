d1cfc000277a830a586579d9480c4bb5684a0c14
leonardo <Leonardo.dellamatrice@gmail.com>
2026-01-04 10:53:35 -0300

    Build for windows (PowerShel)

--- build.ps1
+++ build.ps1
@@ -1,12 +1,19 @@
 Write-Host "Buildando CheetahPy..." -ForegroundColor Green
 
+python -m venv .venv
+.venv\Scripts\activate
+
+pip install -r requirements.txt
+pip install pyinstaller
+
 pyinstaller app/main.py `
   --onefile `
   --windowed `
   --name CheetahPy `
-  --add-data "app/assets/images/cheetahpy.png;assets/images" `
-  --add-data "app/assets/icons/cheetahpy.ico;assets/icons" `
+  --add-data "app/assets/;assets/" `
   --icon app/assets/icons/cheetahpy.ico `
   --clean
+
+deactivate
 
 Write-Host "Build finalizado!" -ForegroundColor Green
